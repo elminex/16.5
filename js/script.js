87b4a343a6eeff9bfdf5f7a9f968dc1955b7df82
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-04-18 21:25:43 +0200

    Fix js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
-let tweetLink = "https://twitter.com/intent/tweet?text=";
-const quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+const tweetLink = 'https://twitter.com/intent/tweet?text=';
+const quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
 function getQuote() {
     fetch(quoteUrl, { cache: "no-store" })
-        .then(function (resp) {
-            return resp.json();
-        })
+        .then((resp) =>resp.json())
         .then(createTweet);
-    debugger;
 }
 
 function createTweet(input) {
     const data = input[0];
-    const dataElement = documen.createElement('div');
+    const dataElement = document.createElement('div');
     dataElement.innerHTML = data.content;
-    let quoteText = dataElement.innerText.trim();
-    let quoteAuthor = data.title;
+    const quoteText = dataElement.innerText.trim();
+    const quoteAuthor = data.title;
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-    let tweetText = `Quote of the day ${quoteText} Author: ${quoteAuthor}`;
+    const tweetText = `Quote of the day: ${quoteText} Author: ${quoteAuthor}`;
     if (tweetText.lenght > 140) {
         getQuote();
     } else {
-        let tweet = tweetLink + encodeURIComponent(tweetText);
+        const tweet = tweetLink + encodeURIComponent(tweetText);
         document.getElementById('quote').innerText = quoteText;
-        document.getElementById('author').innerText = `Author ${quoteAuthor}`;
+        document.getElementById('author').innerText = `Author: ${quoteAuthor}`;
         document.querySelector('.tweet').setAttribute('href', tweet);
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
     getQuote();
-    document.querySelector('quote-btn').addEventListener('click', () => getQuote());
+    document.querySelector('#quote-btn').addEventListener('click', () => getQuote());
 });
